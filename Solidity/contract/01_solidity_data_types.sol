401746cdcfaacf16edc1b57eedc63d47281fdc38
Racker Joy Jugalbot <rackerjoyjugalbot@MacBook-Air.local>
2025-04-28 09:37:20 +0800

    Commit all changes

--- Solidity/contract/01_solidity_data_types.sol
+++ Solidity/contract/01_solidity_data_types.sol
@@ -13,13 +13,13 @@ contract SolidityDataTypes {
     constructor() {
         balance = 10000;
         owner = address(0);
-        accounts["theblokc"] = 999999;
+        accounts["pctworkshop"] = 999999;
 
         // TODO: print the values above using console.log
          console.log("balance is ", balance);
 
          console.log("owner is ", owner);
 
-         console.log("account is ", accounts["theblokc"]);
+         console.log("account is ", accounts["ptcworkshop"]);
     }
 }
